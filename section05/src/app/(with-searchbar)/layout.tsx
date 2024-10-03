2a12672a9a8ad11bfacf5e643f2c0af5a8a13592
Roy <2circumflex@gmail.com>
2024-10-04 00:49:36 +0900

    5.5) 클라이언트 라우터 캐시

--- section05/src/app/(with-searchbar)/layout.tsx
+++ section05/src/app/(with-searchbar)/layout.tsx
@@ -4,6 +4,9 @@ import Searchbar from "@/components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      {/* <div>{new Date().toLocaleString()}</div> */}
+      {/* https://nextjs.org/docs/app/building-your-application/caching#client-side-router-cache */}
+      {/* 클라이언트 라우터 캐시를 사용하더라도 새로고침, 탭을 껐다 다시 접속하는 경우는 동작하지 않음  */}
       <Suspense fallback={<div>Loading</div>}>
         <Searchbar />
       </Suspense>
